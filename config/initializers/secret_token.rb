dafb0177d23aed24237fc8581d90bb267ea2d284
Tanner Swett <swettt@mail.gvsu.edu>
2012-11-03 15:04:28 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LunchHub::Application.config.secret_token = '293061c170e051589b6b0524ad761c89c031117e1eb3d887ffe7f4d85b998e8056ea74dc56dd13180fdc439d0034d44fd8df7a7ea1b117660b76a02d4f9bda38'
